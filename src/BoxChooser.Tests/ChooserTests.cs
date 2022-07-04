be04c36c57d49ad0afa5742cb45cb8c679e02f11
Mark Bugno <m.bugno@tricentis.com>
2022-07-04 13:57:16 +1000

    Broke up tests to make them easier to follow.

--- src/BoxChooser.Tests/ChooserTests.cs
+++ src/BoxChooser.Tests/ChooserTests.cs
@@ -4,117 +4,281 @@ public class ChooserTests {
     private readonly Chooser chooser;
     public ChooserTests() => chooser = new();
 
-    // This test checks that your intersection calculation is correct.
-    [Theory]
-    [MemberData(nameof(GetData))]
-    public void CalculateIntersection(Box box1, Box box2, int expectedArea) {
+    #region No intersection - space in between
+
+    [Fact]
+    public void CalculateIntersection_Box2IsLeftOfBox1_DoNotIntersect() {
+        // Arrange
+        var box1 = new Box(0, 10, 10, 5, 5, 1);
+        var box2 = new Box(1, 2, 10, 5, 5, 1);
+        var expectedArea = 0;
+
+        // Act
         var actualArea = chooser.CalculateIntersection(box1, box2);
+
+        // Assert
         Assert.Equal(expectedArea, actualArea);
     }
 
-    public static IEnumerable<object[]> GetData() {
-        Box box1, box2;
-        int expectedArea;
+    [Fact]
+    public void CalculateIntersection_Box2IsAboveBox1_DoNotIntersect() {
+        // Arrange
+        var box1 = new Box(0, 10, 10, 5, 5, 1);
+        var box2 = new Box(1, 10, 2, 5, 5, 1);
+        var expectedArea = 0;
 
-        // No Intersection
-        // West (box2 is West of box1; i.e., box2 is to the left of box1)
-        box1 = new(0, 10, 10, 5, 5, 1);
-        box2 = new(1,  2, 10, 5, 5, 1);
-        expectedArea = 0;
-        yield return new object[] { box1, box2, expectedArea };
-        // North (box2 is North of box1; i.e., box2 is above box1)
-        box1 = new(0, 10, 10, 5, 5, 1);
-        box2 = new(1, 10,  2, 5, 5, 1);
-        expectedArea = 0;
-        yield return new object[] { box1, box2, expectedArea };
-        // East
-        box1 = new(0, 10, 10, 5, 5, 1);
-        box2 = new(1, 18, 10, 5, 5, 1);
-        expectedArea = 0;
-        yield return new object[] { box1, box2, expectedArea };
-        // South
-        box1 = new(0, 10, 10, 5, 5, 1);
-        box2 = new(1, 10, 18, 5, 5, 1);
-        expectedArea = 0;
-        yield return new object[] { box1, box2, expectedArea };
+        // Act
+        var actualArea = chooser.CalculateIntersection(box1, box2);
 
-        // Adjacent (No Intersection)
-        // W
-        box1 = new(0, 10, 10, 5, 5, 1);
-        box2 = new(1,  5, 10, 5, 5, 1);
-        expectedArea = 0;
-        yield return new object[] { box1, box2, expectedArea };
-        // N
-        box1 = new(0, 10, 10, 5, 5, 1);
-        box2 = new(1, 10,  5, 5, 5, 1);
-        expectedArea = 0;
-        yield return new object[] { box1, box2, expectedArea };
-        // E
-        box1 = new(0, 10, 10, 5, 5, 1);
-        box2 = new(1, 15, 10, 5, 5, 1);
-        expectedArea = 0;
-        yield return new object[] { box1, box2, expectedArea };
-        // S
-        box1 = new(0, 10, 10, 5, 5, 1);
-        box2 = new(1, 10, 15, 5, 5, 1);
-        expectedArea = 0;
-        yield return new object[] { box1, box2, expectedArea };
-
-        // Overlap
-        // W
-        box1 = new(0, 10, 10, 5, 5, 1);
-        box2 = new(1,  7, 10, 5, 5, 1);
-        expectedArea = 10;
-        yield return new object[] { box1, box2, expectedArea };
-        // N
-        box1 = new(0, 10, 10, 5, 5, 1);
-        box2 = new(1, 10,  7, 5, 5, 1);
-        expectedArea = 10;
-        yield return new object[] { box1, box2, expectedArea };
-        // E
-        box1 = new(0, 10, 10, 5, 5, 1);
-        box2 = new(1, 13, 10, 5, 5, 1);
-        expectedArea = 10;
-        yield return new object[] { box1, box2, expectedArea };
-        // S
-        box1 = new(0, 10, 10, 5, 5, 1);
-        box2 = new(1, 10, 13, 5, 5, 1);
-        expectedArea = 10;
-        yield return new object[] { box1, box2, expectedArea };
-        // NW
-        box1 = new(0, 10, 10, 5, 5, 1);
-        box2 = new(1,  7,  7, 5, 5, 1);
-        expectedArea = 4;
-        yield return new object[] { box1, box2, expectedArea };
-        // SW
-        box1 = new(0, 10, 10, 5, 5, 1);
-        box2 = new(1,  7, 13, 5, 5, 1);
-        expectedArea = 4;
-        yield return new object[] { box1, box2, expectedArea };
-        // NE
-        box1 = new(0, 10, 10, 5, 5, 1);
-        box2 = new(1, 13,  7, 5, 5, 1);
-        expectedArea = 4;
-        yield return new object[] { box1, box2, expectedArea };
-        // SE
-        box1 = new(0, 10, 10, 5, 5, 1);
-        box2 = new(1, 13, 13, 5, 5, 1);
-        expectedArea = 4;
-        yield return new object[] { box1, box2, expectedArea };
-        // Overlap
-        box1 = new(0, 10, 10, 5, 5, 1);
-        box2 = new(1, 10, 10, 5, 5, 1);
-        expectedArea = 25;
-        yield return new object[] { box1, box2, expectedArea };
-        // Inside
-        box1 = new(0, 10, 10, 5, 5, 1);
-        box2 = new(1, 11, 11, 3, 3, 1);
-        expectedArea = 9;
-        yield return new object[] { box1, box2, expectedArea };
-        // Outside
-        box1 = new(0, 10, 10, 5, 5, 1);
-        box2 = new(1,  9,  9, 7, 7, 1);
-        expectedArea = 25;
-        yield return new object[] { box1, box2, expectedArea };
+        // Assert
+        Assert.Equal(expectedArea, actualArea);
     }
+
+    [Fact]
+    public void CalculateIntersection_Box2IsRightOfBox1_DoNotIntersect() {
+        // Arrange
+        var box1 = new Box(0, 10, 10, 5, 5, 1);
+        var box2 = new Box(1, 18, 10, 5, 5, 1);
+        var expectedArea = 0;
+
+        // Act
+        var actualArea = chooser.CalculateIntersection(box1, box2);
+
+        // Assert
+        Assert.Equal(expectedArea, actualArea);
+    }
+
+    [Fact]
+    public void CalculateIntersection_Box2IsBelowBox1_DoNotIntersect() {
+        // Arrange
+        var box1 = new Box(0, 10, 10, 5, 5, 1);
+        var box2 = new Box(1, 10, 18, 5, 5, 1);
+        var expectedArea = 0;
+
+        // Act
+        var actualArea = chooser.CalculateIntersection(box1, box2);
+
+        // Assert
+        Assert.Equal(expectedArea, actualArea);
+    }
+
+    #endregion
+
+    #region No intersection - adjacent
+
+    [Fact]
+    public void CalculateIntersection_Box2TouchesLeftOfBox1_DoNotIntersect() {
+        // Arrange
+        var box1 = new Box(0, 10, 10, 5, 5, 1);
+        var box2 = new Box(1, 5, 10, 5, 5, 1);
+        var expectedArea = 0;
+
+        // Act
+        var actualArea = chooser.CalculateIntersection(box1, box2);
+
+        // Assert
+        Assert.Equal(expectedArea, actualArea);
+    }
+
+    [Fact]
+    public void CalculateIntersection_Box2TouchesTopOfBox1_DoNotIntersect() {
+        // Arrange
+        var box1 = new Box(0, 10, 10, 5, 5, 1);
+        var box2 = new Box(1, 10, 5, 5, 5, 1);
+        var expectedArea = 0;
+
+        // Act
+        var actualArea = chooser.CalculateIntersection(box1, box2);
+
+        // Assert
+        Assert.Equal(expectedArea, actualArea);
+    }
+
+    [Fact]
+    public void CalculateIntersection_Box2TouchesRightOfBox1_DoNotIntersect() {
+        // Arrange
+        var box1 = new Box(0, 10, 10, 5, 5, 1);
+        var box2 = new Box(1, 15, 10, 5, 5, 1);
+        var expectedArea = 0;
+
+        // Act
+        var actualArea = chooser.CalculateIntersection(box1, box2);
+
+        // Assert
+        Assert.Equal(expectedArea, actualArea);
+    }
+
+    [Fact]
+    public void CalculateIntersection_Box2TouchesBottomOfBox1_DoNotIntersect() {
+        // Arrange
+        var box1 = new Box(0, 10, 10, 5, 5, 1);
+        var box2 = new Box(1, 10, 15, 5, 5, 1);
+        var expectedArea = 0;
+
+        // Act
+        var actualArea = chooser.CalculateIntersection(box1, box2);
+
+        // Assert
+        Assert.Equal(expectedArea, actualArea);
+    }
+
+    #endregion
+
+    #region Intersects
+
+    [Fact]
+    public void CalculateIntersection_Box2IntersectsLeftSideOfBox1_Intersects() {
+        // Arrange
+        var box1 = new Box(0, 10, 10, 5, 5, 1);
+        var box2 = new Box(1, 7, 10, 5, 5, 1);
+        var expectedArea = 10;
+
+        // Act
+        var actualArea = chooser.CalculateIntersection(box1, box2);
+
+        // Assert
+        Assert.Equal(expectedArea, actualArea);
+    }
+
+    [Fact]
+    public void CalculateIntersection_Box2IntersectsTopSideOfBox1_Intersects() {
+        // Arrange
+        var box1 = new Box(0, 10, 10, 5, 5, 1);
+        var box2 = new Box(1, 10, 7, 5, 5, 1);
+        var expectedArea = 10;
+
+        // Act
+        var actualArea = chooser.CalculateIntersection(box1, box2);
+
+        // Assert
+        Assert.Equal(expectedArea, actualArea);
+    }
+
+    [Fact]
+    public void CalculateIntersection_Box2IntersectsRightSideOfBox1_Intersects() {
+        // Arrange
+        var box1 = new Box(0, 10, 10, 5, 5, 1);
+        var box2 = new Box(1, 13, 10, 5, 5, 1);
+        var expectedArea = 10;
+
+        // Act
+        var actualArea = chooser.CalculateIntersection(box1, box2);
+
+        // Assert
+        Assert.Equal(expectedArea, actualArea);
+    }
+
+    [Fact]
+    public void CalculateIntersection_Box2IntersectsBottomSideOfBox1_Intersects() {
+        // Arrange
+        var box1 = new Box(0, 10, 10, 5, 5, 1);
+        var box2 = new Box(1, 10, 13, 5, 5, 1);
+        var expectedArea = 10;
+
+        // Act
+        var actualArea = chooser.CalculateIntersection(box1, box2);
+
+        // Assert
+        Assert.Equal(expectedArea, actualArea);
+    }
+
+    [Fact]
+    public void CalculateIntersection_Box2IntersectsTopLeftCornerOfBox1_Intersects() {
+        // Arrange
+        var box1 = new Box(0, 10, 10, 5, 5, 1);
+        var box2 = new Box(1, 7, 7, 5, 5, 1);
+        var expectedArea = 4;
+
+        // Act
+        var actualArea = chooser.CalculateIntersection(box1, box2);
+
+        // Assert
+        Assert.Equal(expectedArea, actualArea);
+    }
+
+    [Fact]
+    public void CalculateIntersection_Box2IntersectsBottomLeftCornerOfBox1_Intersects() {
+        // Arrange
+        var box1 = new Box(0, 10, 10, 5, 5, 1);
+        var box2 = new Box(1, 7, 13, 5, 5, 1);
+        var expectedArea = 4;
+
+        // Act
+        var actualArea = chooser.CalculateIntersection(box1, box2);
+
+        // Assert
+        Assert.Equal(expectedArea, actualArea);
+    }
+
+    [Fact]
+    public void CalculateIntersection_Box2IntersectsTopRightCornerOfBox1_Intersects() {
+        // Arrange
+        var box1 = new Box(0, 10, 10, 5, 5, 1);
+        var box2 = new Box(1, 13, 7, 5, 5, 1);
+        var expectedArea = 4;
+
+        // Act
+        var actualArea = chooser.CalculateIntersection(box1, box2);
+
+        // Assert
+        Assert.Equal(expectedArea, actualArea);
+    }
+
+    [Fact]
+    public void CalculateIntersection_Box2IntersectsBottomRightCornerOfBox1_Intersects() {
+        // Arrange
+        var box1 = new Box(0, 10, 10, 5, 5, 1);
+        var box2 = new Box(1, 13, 13, 5, 5, 1);
+        var expectedArea = 4;
+
+        // Act
+        var actualArea = chooser.CalculateIntersection(box1, box2);
+
+        // Assert
+        Assert.Equal(expectedArea, actualArea);
+    }
+
+    [Fact]
+    public void CalculateIntersection_Box2MatchesBox1_Intersects() {
+        // Arrange
+        var box1 = new Box(0, 10, 10, 5, 5, 1);
+        var box2 = new Box(1, 10, 10, 5, 5, 1);
+        var expectedArea = 25;
+
+        // Act
+        var actualArea = chooser.CalculateIntersection(box1, box2);
+
+        // Assert
+        Assert.Equal(expectedArea, actualArea);
+    }
+
+    [Fact]
+    public void CalculateIntersection_Box2IsInsideOfBox1_Intersects() {
+        // Arrange
+        var box1 = new Box(0, 10, 10, 5, 5, 1);
+        var box2 = new Box(1, 11, 11, 3, 3, 1);
+        var expectedArea = 9;
+
+        // Act
+        var actualArea = chooser.CalculateIntersection(box1, box2);
+
+        // Assert
+        Assert.Equal(expectedArea, actualArea);
+    }
+
+    [Fact]
+    public void CalculateIntersection_Box2ContainsBox1_Intersects() {
+        // Arrange
+        var box1 = new Box(0, 10, 10, 5, 5, 1);
+        var box2 = new Box(1, 9, 9, 7, 7, 1);
+        var expectedArea = 25;
+
+        // Act
+        var actualArea = chooser.CalculateIntersection(box1, box2);
+
+        // Assert
+        Assert.Equal(expectedArea, actualArea);
+    }
+
+    #endregion
 }
